33f238a84a3bc2aeb641732d26e582523fcd012b
chinakr <chinakr@gmail.com>
2016-05-31 00:39:11 +0800

    Optimize Capistrano deployment.

--- Gemfile
+++ Gemfile
@@ -28,7 +28,14 @@ gem 'jbuilder', '~> 2.0'
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
-gem 'capistrano-rails', group: :development
+#gem 'capistrano-rails', group: :development
+group :development do
+  gem 'capistrano', '~> 3.5'
+  gem 'capistrano-rvm'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano3-puma', github: 'seuros/capistrano-puma'
+end
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/deploy.rb
+++ config/deploy.rb
@@ -26,16 +26,26 @@ set :deploy_to, '/home/deploy/pd.chejj.com.cn/chejjpd'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'shared/log', 'shared/pids', 'shared/sockets', 'tmp/cache', 'tmp/pids', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :puma_state, "#{shared_path}/shared/pids/puma.state"
+set :puma_pid, "#{shared_path}/shared/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/shared/sockets/puma.sock"
+set :puma_default_control_app, "unix://#{shared_path}/shared/sockets/pumactl.sock"
+set :puma_conf, "#{shared_path}/config/puma.rb"
+set :puma_threads, [5, 5]
+set :puma_workers, 2
+set :puma_jungle_conf, '/etc/puma.conf'
 
 namespace :deploy do
 
